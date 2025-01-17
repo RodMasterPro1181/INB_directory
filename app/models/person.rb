2c4c9ab4bb2f569b65c35350f915157f59ab2686
rodrigo <rodrigo@gmail.com>
2025-01-17 11:18:10 -0600

    Hacemos que pueda eliminar los registros

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,10 @@
 class Person < ApplicationRecord
-#Rama1, vamos a validar que la informacion no este en blanco
+#Rama1, vamos a validar que la informacion no este en blanco, commit1
 validates :name, presence: true 
 validates :last_name, presence: true 
+
+#Rama1,vamos a verificar que ya se pueden eliminar los registros, commit2
+person =Person.find(1)
+person.distroy!
+
 end
