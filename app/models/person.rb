ff18dd5300afe84c224d6e730582e01e5f9b2ba0
rodrigo <rodrigo@gmail.com>
2025-01-17 14:16:09 -0600

    Restriccion de 20 caracteres

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,8 @@
 class Person < ApplicationRecord
 #Rama1, vamos a validar que la informacion no este en blanco, commit1
-validates :name, presence: true 
-validates :last_name, presence: true 
+validates :name, presence: true, length:{maximum: 20}
+validates :last_name, presence: true, length:{maximum: 20}
 
 #Rama1,vamos a verificar que ya se pueden eliminar los registros, commit2
-person =Person.find(1)
-person.distroy!
 
 end
