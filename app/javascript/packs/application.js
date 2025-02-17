0c250238653208d1382bdb9d293c739ffff00537
rodrigo <rodrigo@gmail.com>
2025-02-17 16:57:48 -0600

    Iconos de index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
-import "bootstrap";
-import "../stylesheets/application"; // Importa los estilos SCSS
+import "bootstrap"
+import "../stylesheets/application.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import Rails from "@rails/ujs";
 Rails.start();
 import "@fortawesome/fontawesome-free/css/all.css";
