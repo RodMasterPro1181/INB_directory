7bd7818d1eac5fe8cb83a8e70ff3da1dc1c653bf
rodrigo <rodrigo@gmail.com>
2025-02-19 16:57:29 -0600

    Se arreglo el diseño, pero los iconos todavia no

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import "bootstrap"
 import "../stylesheets/application.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
 import Rails from "@rails/ujs";
 Rails.start();
+import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/css/all.css";
