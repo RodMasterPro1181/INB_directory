598d4fa36c9f534f2b94b5c46b7a09268b885f8e
rodrigo <rodrigo@gmail.com>
2025-02-24 15:38:05 -0600

    Eliminar arreglado pero diseño al 7o o 80%

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,4 @@
-import "bootstrap"
 import "../stylesheets/application.scss";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import Rails from "@rails/ujs";
-Rails.start();
-import "@fortawesome/fontawesome-free/css/all.css";
+Rails.start()
 import "@fortawesome/fontawesome-free/css/all.css";
