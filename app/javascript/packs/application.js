0b344799a4000f1e0f4ca5e03f0e9288f48cdf68
rodrigo <rodrigo@gmail.com>
2025-03-07 22:28:15 -0600

    Todo al 90%

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,26 @@ import "../stylesheets/application.scss";
 import Rails from "@rails/ujs";
 Rails.start()
 import "@fortawesome/fontawesome-free/css/all.css";
-
+document.addEventListener("DOMContentLoaded", function () {
+    const languageDropdown = document.querySelector(".language-dropdown");
+    const languageMenu = document.querySelector(".dropdown-menu");
+  
+    function toggleLanguageMenu() {
+      languageMenu.classList.toggle("active");
+    }
+  
+    languageDropdown.addEventListener("click", function (event) {
+      event.stopPropagation(); // Evita que se cierre inmediatamente
+      toggleLanguageMenu();
+    });
+  
+    // Cierra el menú si el usuario hace clic fuera de él
+    document.addEventListener("click", function (event) {
+      if (!languageDropdown.contains(event.target)) {
+        languageMenu.classList.remove("active");
+      }
+    });
+  });
+  
   
   
