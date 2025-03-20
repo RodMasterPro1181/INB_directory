46c2a0a0f2631d86a123aa94a81ce7764464b70e
rodrigo <rodrigo@gmail.com>
2025-03-20 17:19:02 -0600

    Emails 40% (Implementando Javascript)

--- app/javascript/controllers/emails_controller.js
+++ app/javascript/controllers/emails_controller.js
@@ -1,13 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["emails", "template"];
+  static targets = ["emails"];
 
   add(event) {
     event.preventDefault();
 
-    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
-    this.emailsTarget.insertAdjacentHTML("beforeend", content);
+    // Clonar un campo de email existente o crear uno nuevo
+    let newEmailField = document.createElement("div");
+    newEmailField.innerHTML = `
+      <input type="text" name="person[emails][]" class="input-field" required>
+      <button type="button" data-action="click->email#remove">❌</button>
+    `;
+
+    this.emailsTarget.appendChild(newEmailField);
+  }
+
+  remove(event) {
+    event.preventDefault();
+    event.target.closest("div").remove();
   }
 }
 
